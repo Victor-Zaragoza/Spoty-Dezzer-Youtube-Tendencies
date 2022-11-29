9f206f9dde525a3188a33d499e7220953b827247
Victor-Zaragoza <zaragoza_victorr@outlook.com>
2022-11-28 22:13:57 -0600

    First commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {Heroe} from '../heroe'
+import { HeroeService } from '../shared/heroe.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  misHeroes:Heroe[]=[];
+
+  constructor(public miservicio:HeroeService) { 
+    console.log("constructor de Heroes");
+  }
+
+  ngOnInit(): void {
+    console.log("ngOnit de Heroes");
+    this.misHeroes=this.miservicio.getHeroes();
+    console.log(this.misHeroes);
+  }
+
+}
