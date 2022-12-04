1e957de8ed9f7ebd22190f0ff1e47d7d6e20e1bc
Victor-Zaragoza <zaragoza_victorr@outlook.com>
2022-12-04 14:19:30 -0600

    Api1

---
+++ server/src/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const app = express();
+
+const fs = require ('fs');
+
+
+
+
+app.all('/*', function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
+app.use(cors());
+app.use(bodyparser.json());
+
+app.get('/user/:id', (req, res) => {
+    let gID = req.params.id;
+    
+    console.log(gID);
+        
+        
+    res.status(200).send({status: "ok", message: "1 User data", data: gID});
+      
+}) 
+
+app.get('/saveinfo/:id', (req,res) => {
+    let gID = req.params.id;
+    
+    fs.appendFile('//192.168.56.1/prueba/docuemnto3.txt', 'texto que contiene el documento etc', 
+        (error) =>{
+            if(error)
+                throw error;
+            console.log("archivo creado: ", gID);
+        })
+})
+
+
+app.listen(3000, (req, res)=>{
+    console.log('SERVER RUNNING IN http://localhost:3000');
+});
